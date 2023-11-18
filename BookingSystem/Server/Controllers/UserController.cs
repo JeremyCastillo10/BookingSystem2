fd8896fb9224a3c295c3ce3086ba685e9ee365fb
jeremy castillo taveras <jeremycastillo901@gmail.com>
2023-11-18 14:05:45 -0400

    adding Usercontroller

---
+++ BookingSystem/Server/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using BookingSystem.Server.Data;
+using BookingSystem.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+
+        public UserController(Contexto contexto)
+        {
+           _contexto = contexto;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<ApplicationUser>>> GetUsers()
+        {
+            var response = await _contexto.UsuarioAplicacion.ToListAsync();
+            return Ok(response);
+        }
+    }
+}
